11201546d862cf8ec48b8407d843641f259c556b
PAVAN-TELANAKULA <t.pavan08@gmail.com>
2022-01-31 13:14:27 +0530

    changed

--- src/com/uks/core/day8/ThreadDemo1.java
+++ src/com/uks/core/day8/ThreadDemo1.java
@@ -2,14 +2,14 @@ package com.uks.core.day8;
 
 public class ThreadDemo1 extends Thread{
 
-	public synchronized void run(){
+	public void run(){
 		for (int i=1;i<=5000;i++){
 			System.out.println("Thread<"+Thread.currentThread().getName()+"> : "+i+"\n");
 
 			if(i%50 == 0){
 				try {
 					Thread.sleep(100);
-					this.notifyAll();
+					//this.notifyAll();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
--- src/com/uks/core/day8/ThreadDemo2.java
+++ src/com/uks/core/day8/ThreadDemo2.java
@@ -2,14 +2,14 @@ package com.uks.core.day8;
 
 public class ThreadDemo2 implements Runnable{
 	@Override
-	public synchronized void run() {
+	public void run() {
 		for (int i=1;i<=5000;i++){
 			System.out.println("Thread<"+Thread.currentThread().getName()+"> : "+i+"\n");
 
 			if (i%75 == 0){
 				try {
 					Thread.sleep(200);
-					this.notifyAll();
+					//this.notifyAll();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
